8f8818083fb27061133c68632e487304cd394859
GeraDes10 <pmz11_garcia@hotmail.com>
2022-10-14 22:31:03 -0500

    useEffect y El viejo React-Router-Dom

--- src/Pages/PageHome.jsx
+++ src/Pages/PageHome.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import Cabezera from '../componets/Cabezera';
+import React from "react";
+import Cabezera from "../componets/Cabezera";
 
 const PageHome = () => {
+
+  const [equipo, setEquipo] = React.useState([])
+
+  React.useEffect(() =>{
+    obtenerDatos();
+  }, [])
+  
+  const obtenerDatos = async () =>{
+    const data = await fetch('https://jsonplaceholder.typicode.com/users')
+    const users = await data.json()
+    //console.log(users)
+    setEquipo(users)
+  }
   return (
-    <><Cabezera /></>
-  )
-}
+    <>
+      <Cabezera />
+      <h1>Pagina Inicial</h1>
+      <h3>Nuestro usuarios actualmente:</h3>
+      <ul>
+        {
+          equipo.map(item =>(
+            <li key={item.id}>{item.name} - {item.username}</li>
+          ))
+        }
+      </ul>
+    </>
+  );
+};
 
 export default PageHome;
--- src/componets/Cabezera.jsx
+++ src/componets/Cabezera.jsx
@@ -7,12 +7,12 @@ const Cabezera = () => {
       <head>
         <nav>
           <ul>
-            <li><Link to='/PracUno'>Practica Uno</Link></li>
-            <li><Link to='/PracDos'>Practica Dos</Link></li>
-            <li><Link to='/PracTres'>Practica Tres</Link></li>
-            <li><Link to='/PracCuatro'>Practica Cuatro</Link></li>
-            <li><Link to='/PracCinco'>Practica Cinco</Link></li>
-            <li><Link to='/PracSeis'>Practica Seis</Link></li>
+            <li><Link to='/PracUno' className="btn btn0dark">Practica Uno</Link></li>
+            <li><Link to='/PracDos' className="btn btn0dark">Practica Dos</Link></li>
+            <li><Link to='/PracTres' className="btn btn0dark">Practica Tres</Link></li>
+            <li><Link to='/PracCuatro' className="btn btn0dark">Practica Cuatro</Link></li>
+            <li><Link to='/PracCinco' className="btn btn0dark">Practica Cinco</Link></li>
+            <li><Link to='/PracSeis' className="btn btn0dark">Practica Seis</Link></li>
           </ul>
         </nav>
       </head>
